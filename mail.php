c4a447449ddec6fea5d95c904ccc6c8061e581c9
rachelwitherspoon <64097394+rachelwitherspoon@users.noreply.github.com>
2020-05-03 18:12:30 -0400

    contact page

--- mail.php
+++ mail.php
@@ -1,9 +1,6 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "witherspoonr@guilford.edu";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='thankyou.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+<?php
+	$to = "witherspoonr@guilford.edu";
+	$subject = "Email from my website";
+	$body = "the user typed in ...";
+	mail($to, $subject, $body);
+?>
