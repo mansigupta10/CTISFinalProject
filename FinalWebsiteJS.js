be004399431195eab4cf6a9ee2794020e208a4c0
nahte-ethan <santanae@guilford.edu>
2020-05-04 12:33:35 -0400

    Add drop down menu

---
+++ FinalWebsiteJS.js
@@ -0,0 +1,9 @@
+
+essentialsbutton = document.getElementById("essentials");
+dropdown = document.getElementById("dropDownMenu");
+
+essentialsbutton.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+    dropdown.style.display = "flex";
+}
