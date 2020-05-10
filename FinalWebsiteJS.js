7f6d8e1b18d1703b46014b69b8e01ec0e5a14278
rachelwitherspoon <64097394+rachelwitherspoon@users.noreply.github.com>
2020-05-10 16:20:26 -0400

    fix contact form responses based on May8 class

--- FinalWebsiteJS.js
+++ FinalWebsiteJS.js
@@ -8,3 +8,16 @@ function onButtonClick() {
     dropdown.style.display = "flex";
 }
 
+/*contact page*/
+name=document.getElementById("name");
+email=document.getElementById("email");
+comments=document.getElementById("comments");
+
+function formfunction() {
+    document.write("<h1>");
+    document.write("Thank you!");
+    document.write("</h1>");
+    document.write("<h2>");
+    document.write("Your response has been recorded");
+    document.write("</h2>");
+}
