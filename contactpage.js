05e6057dd29845a4feed20deef5d17264a6062d4
rachelwitherspoon <64097394+rachelwitherspoon@users.noreply.github.com>
2020-05-11 11:39:22 -0400

    add js pages

---
+++ contactpage.js
@@ -0,0 +1,13 @@
+/*contact page*/
+name=document.getElementById("name");
+email=document.getElementById("email");
+comments=document.getElementById("comments");
+
+function formfunction() {
+    document.write("<h1>");
+    document.write("Thank you!");
+    document.write("</h1>");
+    document.write("<h2>");
+    document.write("Your response has been recorded");
+    document.write("</h2>");
+}
